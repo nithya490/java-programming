669a4e7895ce3b1efdc704702db2109bc6358b79
nithya490 <72901263+nithya490@users.noreply.github.com>
2020-10-23 20:05:18 +0530

    incrementing the value

---
+++ Increment.java
@@ -0,0 +1,29 @@
+class Increment{
+public static void main (String args[]){
+int a = 10;
+int b = ++a;
+int c = b++;
+System.out.println(c);
+System.out.println(c++);
+System.out.println(++b);
+System.out.println(a++);
+System.out.println(++c);
+System.out.println(b++);
+System.out.println(++a);
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
