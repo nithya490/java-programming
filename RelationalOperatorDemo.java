ec251d3ad4a81e9deaf93c264e33bcb0987ae125
nithya490 <72901263+nithya490@users.noreply.github.com>
2020-10-24 13:42:06 +0530

    using relational operator

---
+++ RelationalOperatorDemo.java
@@ -0,0 +1,8 @@
+class RelationalOperatorDemo{
+public static void main(String[]args){
+int x=50,y=60,z=70;
+System.out.println("x==y:"+(x==y));
+System.out.println("x==y:"+(x!=y));
+System.out.println("x==y:"+(x<y));
+}
+}
