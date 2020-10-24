14051fec47b1d206d119eaad4a5f2b29f3d2a651
nithya490 <72901263+nithya490@users.noreply.github.com>
2020-10-24 13:42:40 +0530

    another example

---
+++ RelationalOperator.java
@@ -0,0 +1,8 @@
+class RelationalOperator{
+public static void main(String[]args){
+int x=50,y=60,z=60;
+System.out.println("y<=z:"+(y<=z));
+System.out.println("y>z:"+(y>z));
+System.out.println("y>=z:"+(y>=z));
+}
+}
