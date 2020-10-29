fb0e00f6c319fec84695ec7aa98df0cafcab5bfa
nithya490 <72901263+nithya490@users.noreply.github.com>
2020-10-29 23:57:38 +0530

    return statement

---
+++ Returnstatementdemo.java
@@ -0,0 +1,13 @@
+class Returnstatementdemo
+{
+public static void main(String args[])
+{
+int result= add(3,3);
+System.out.print( result);
+}
+static int add(int a ,int b){
+int c= a+b;
+return c;
+}
+}
+
