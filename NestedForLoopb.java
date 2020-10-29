f00745f9fcb1e96450fc23f8adebd50c1ecafed9
nithya490 <72901263+nithya490@users.noreply.github.com>
2020-10-29 23:53:37 +0530

    nested for loop with break statement

---
+++ NestedForLoopb.java
@@ -0,0 +1,16 @@
+class NestedForLoopb{
+public static void main(String args[]){
+for(int i=1;i<=4;i++)
+{
+for(int j=1;j<=4;j++){
+{
+System.out.print(j+" ");
+}
+if(j==3){
+break;
+}
+}
+System.out.println();
+}
+}
+}
