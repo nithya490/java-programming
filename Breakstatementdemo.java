29bbb1938b688e1903ec8db44227d43caa71f47c
nithya490 <72901263+nithya490@users.noreply.github.com>
2020-10-29 23:48:12 +0530

    breaking statement

---
+++ Breakstatementdemo.java
@@ -0,0 +1,15 @@
+class Breakstatementdemo
+{
+public static void main(String args[])
+{
+for(int i=0;i<5;i++)
+{
+if(i==3)
+{
+System.out.print("breaking the loop.... at 3");
+break;
+}
+System.out.println(i);
+}
+}
+}
