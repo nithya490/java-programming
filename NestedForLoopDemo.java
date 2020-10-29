1b2146b45c2a33b9c4715c2ca7c8c7ead4b13845
nithya490 <72901263+nithya490@users.noreply.github.com>
2020-10-29 23:56:17 +0530

    another ex of nested for loop

---
+++ NestedForLoopDemo.java
@@ -0,0 +1,18 @@
+class NestedForLoopDemo{
+public static void main(String args[]){
+for(int i=1;i<=3;i++)
+System.out.print(i+" ");
+System.out.println();
+{
+for(int j=4;j<=6;j++)
+{ System.out.print(j+" ");
+}
+System.out.println();
+}
+for(int g=7;g<=9;g++)
+{ System.out.print(g+" ");
+}
+System.out.println();
+}
+}
+ 
